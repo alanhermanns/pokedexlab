c35ca5ed863407946a71ed2f20d65b90017f9c21
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-01 10:00:37 -0700

    workingstuff

--- App.js
+++ App.js
@@ -9,14 +9,13 @@ export class App extends Component{
         const header = new Header;
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
-        debugger
+        
         let pokemon = { props: [] };
         const pokeList = new PokeList(pokemon);
         const pokeListDOM = pokeList.renderDOM();
         dom.append(pokeListDOM);
         const rawPokeJsonResponse = await goGetPokemonGo();
 
-        debugger
         const resultantPokemonArray = rawPokeJsonResponse.results;
         pokeList.update({ props : resultantPokemonArray });
 
