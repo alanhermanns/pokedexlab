d8ad2b336fa89d5c1c7e989dceaefc2bda00ec30
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-12 16:45:15 -0800

    server change

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
